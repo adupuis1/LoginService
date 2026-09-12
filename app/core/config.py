e4275870ceb0719b750f2bb6b63a28b2b526aa3f
Anatole Dupuis <anatole.dupuis@gmail.com>
2026-09-12 17:58:55 -0400

    ADD /core/security.py and ADD jwt

--- app/core/config.py
+++ app/core/config.py
@@ -7,7 +7,10 @@ class Settings(BaseSettings):
         env_ignore_empty=True, 
         extra = "ignore",
     )
-
+    SECRET_KEY: str
+    # 60 minutes * 24 hours * 8 days = 8 days
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
+    API_V1_STR: str = "/api/v1"
     PROJECT_NAME: str ="Login Service"
     DATABASE_URL: str = "postgresql+psycopg://postgres:dev@localhost:5432/postgres"
 
---
+++ app/core/security.py
@@ -0,0 +1,24 @@
+from datetime import UTC, datetime, timedelta
+from typing import Any
+
+import jwt
+from pwdlib import PasswordHash
+from pwdlib.hashers.argon2 import Argon2Hasher
+from pwdlib.hashers.bcrypt import BcryptHasher
+
+from app.core.config import settings
+
+password_hash = PasswordHash(
+    (
+        Argon2Hasher(),
+        BcryptHasher
+    )
+)
+
+ALGORITHM = "HS256"
+
+def create_access_token(subject: str | Any, expires_delta: timedelta) -> str:
+    expire = datetime.now(UTC) + expires_delta
+    to_encode = {"exp": expire, "sub": str(subject)}
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
